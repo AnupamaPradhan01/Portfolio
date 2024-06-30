47baf751fc83e9511ac6263c0e9e9bbe8ec965a6
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2024-06-30 17:06:15 +0530

    created skills page

--- portfolio/myapp/views.py
+++ portfolio/myapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     return render(request,"myapp/home.html")
 
+# skill page
+def skill(request):
+    return render(request,"myapp/skills.html")
+
