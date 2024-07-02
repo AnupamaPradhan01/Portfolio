4177e77cdc52d09080d222b311af677a896768e8
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2024-07-02 15:39:44 +0530

    made some changes

--- portfolio/myapp/views.py
+++ portfolio/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Project
+from django.core.paginator import Paginator
 
 # home page.
 def home(request):
@@ -11,7 +12,11 @@ def skill(request):
 
 # projects page
 def projects(request):
-    projects=Project.published.all()
+    project_list=Project.published.all()
+    # pagination with 2 posts per page
+    paginator=Paginator(project_list,2)
+    page_number=request.GET.get('page',1)
+    projects=paginator.page(page_number)
     return render(request,"myapp/projects.html",{'projects':projects}) 
 
 
